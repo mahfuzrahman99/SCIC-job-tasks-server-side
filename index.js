f371bcaa9287041e82dffbf60a3928c592059667
mahfuzurrahman99 <mahfuzurrahmanshabbir@gmail.com>
2023-12-26 01:47:00 +0600

    project copleted

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 2000;
 
-app.use(cors({
-    origin:[
-        "https://scic-assignment-eight-job-task-mahfuz.surge.sh",
-        "http://localhost:5173",
-        "http://localhost:5174",
-    ]
-}));
+app.use(
+  cors({
+    origin: [
+      "https://scic-assignment-eight-job-task-mahfuz.surge.sh",
+      "http://localhost:5173",
+      "http://localhost:5174",
+    ],
+  })
+);
 app.use(express.json());
 
 const uri =
@@ -27,52 +29,100 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const taskCollection = client.db("taskDB").collection("task");
+    const usersCollection = client.db("taskDB").collection("users");
 
+    // user post
+    app.post("/users", async (req, res) => {
+      try {
+        const newBrand = req.body;
+        const result = await usersCollection.insertOne(newBrand);
+        res.send(result);
+      } catch (error) {
+        res.send(error);
+      }
+    });
     // add task post
     app.post("/task", async (req, res) => {
-      const newBrand = req.body;
-      const result = await taskCollection.insertOne(newBrand);
-      res.send(result);
+      try {
+        const newBrand = req.body;
+        const result = await taskCollection.insertOne(newBrand);
+        res.send(result);
+      } catch (error) {
+        res.send(error);
+      }
     });
     // add task get all
     app.get("/task", async (req, res) => {
-      const result = await taskCollection.find().toArray();
-      res.send(result);
+      try {
+        const result = await taskCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        res.send(error);
+      }
     });
     // add task get specific id
     app.get("/task/:id", async (req, res) => {
-      const id = req.params.id;
-      const result = await taskCollection.findOne({ _id: new ObjectId(id) });
-      res.send(result);
+      try {
+        const id = req.params.id;
+        const result = await taskCollection.findOne({ _id: new ObjectId(id) });
+        res.send(result);
+      } catch (error) {
+        res.send(error);
+      }
     });
     // update task
     app.patch("/task/:id", async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const options = { upsert: true };
-      const updatesTask = req.body;
-      const product = {
-        $set: {
-          title: updatesTask.title,
-          description: updatesTask.description,
-          deadline: updatesTask.deadline,
-          priority: updatesTask.priority,
-        },
-      };
-      const result = await taskCollection.updateOne(filter, product, options);
-      res.send(result);
-    });
-    // Deleting task data
-    app.delete("/task/:id", async (req, res) => {
-      const query = { _id: new ObjectId(req.params.id) };
-      const result = await taskCollection.deleteOne(query);
-      res.send(result);
+      try {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const updatesTask = req.body;
+        const product = {
+          $set: {
+            title: updatesTask.title,
+            description: updatesTask.description,
+            deadline: updatesTask.deadline,
+            priority: updatesTask.priority,
+          },
+        };
+        const result = await taskCollection.updateOne(filter, product, options);
+        res.send(result);
+      } catch (error) {
+        res.send(error);
+      }
     });
 
-    await client.db("admin").command({ ping: 1 });
+    app.put("/task/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const { status } = req.body;
+
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: { status },
+        };
+        const result = await taskCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } catch (error) {
+        res.send(error.message);
+      }
+    });
+
+    // Deleting task data
+    app.delete("/task/:id", async (req, res) => {
+      try {
+        const query = { _id: new ObjectId(req.params.id) };
+        const result = await taskCollection.deleteOne(query);
+        res.send(result);
+      } catch (error) {
+        res.send(error.message);
+      }
+    });
+
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
